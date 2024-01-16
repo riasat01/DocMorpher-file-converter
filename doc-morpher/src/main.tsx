a30356fa955e1cf5930aa0fc4918da7c93f99863
unknown <riasat01@outlook.com>
2024-01-16 22:57:34 +0600

    project initial structure to development branch

--- doc-morpher/src/main.tsx
+++ doc-morpher/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import AuthProvider from './provider/auth-provider/AuthProvider.tsx'
+import { RouterProvider } from 'react-router-dom'
+import MainRoute from './routes/main-route/MainRoute.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={MainRoute} />
+    </AuthProvider>
   </React.StrictMode>,
 )
