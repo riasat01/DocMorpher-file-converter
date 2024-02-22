29b48ab528a284dda779d6572f7a53a4a3e56a8e
Omer4ruq <omerfaruq4@gmail.com>
2024-02-23 01:29:55 +0600

    adding review page

---
+++ doc-morpher/src/pages/reviews/Reviews.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import ReviewCard from "./ReviewCard";
+import ReviewBox from "./ReviewBox";
+
+const Reviews = () => {
+  const review = useLoaderData();
+  console.log(review);
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-3">
+        {review.map((review) => (
+          <ReviewCard key={review.reviewer_name} review={review}></ReviewCard>
+        ))}
+      </div>
+      <div className="mt-8 ml-72">
+        <ReviewBox></ReviewBox>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
